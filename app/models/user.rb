089ed0be40aafdee20bd02f1c3412679902c0ad0
Yonatan MK <yonatanmk@gmail.com>
2016-08-30 16:08:34 -0400

    Added pins/users association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #checks that a user password contians 6 characters, 1 num, 1 symbol, 1 uppercase letter, 1 lowercase letter
-  validates :password, :presence => true,
-            :on => :create,
-            :format => {:with => /\A.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\!\?\*\$\%\^\&\+\=]).*\Z/ }
+  has_many :pins
+  #checks that a user password contains 6 characters, 1 num, 1 symbol, 1 uppercase letter, 1 lowercase letter
+  #validates :password, :presence => true,
+  #          :on => :create,
+  #          :format => {:with => /\A.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\!\?\*\$\%\^\&\+\=]).*\Z/ }
 end
