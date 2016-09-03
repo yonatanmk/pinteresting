90720768291cf71296aa613a1432800dc970b5eb
Yonatan MK <yonatanmk@gmail.com>
2016-09-03 14:25:41 -0400

    Converted website to cat site

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
     root "pins#index"
     get "about" => "pages#about" #creates about path
-    get "third_page" => "pages#third_page" #creates third_page path
     #get "about" => "pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
